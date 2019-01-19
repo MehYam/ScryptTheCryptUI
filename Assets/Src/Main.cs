d581fd74b78235133b2b58a9352c73613064497d
Kai Arnold <cdn.kai@gmail.com>
2019-01-18 16:04:52 -0800

    hook up button to animated test

--- Assets/Src/Main.cs
+++ Assets/Src/Main.cs
@@ -3,42 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using ScryptTheCrypt;
-using ScryptTheCrypt.Actions;
 
 public class Main : MonoBehaviour
 {
-    void Start()
-    {
-    }
+    [SerializeField] private UnityEngine.UI.Button proceedButton;
     public void RunSmokeTest()
     {
-        // run some simple tests to create and invoke a Game
-        var game = new Game(2112);
-        var player = new GameActor("alice");
-        var player2 = new GameActor("bob");
-        var mob = new GameActor("carly");
-        var mob2 = new GameActor("denise");
-
-        // set targeting and affinities
-        player.AddAction(new ActionChooseRandomTarget(Game.ActorAlignment.Mob));
-        player2.AddAction(new ActionChooseRandomTarget(Game.ActorAlignment.Mob));
-        mob.AddAction(new ActionChooseRandomTarget(Game.ActorAlignment.Player));
-        mob2.AddAction(new ActionChooseRandomTarget(Game.ActorAlignment.Player));
-
-        player.AddAction(new ActionAttack());
-        player2.AddAction(new ActionAttack());
-        mob.AddAction(new ActionAttack());
-        mob2.AddAction(new ActionAttack());
-
-        player.Weapon = new GameWeapon("alice's axe", 22);
-        player2.Weapon = new GameWeapon("bob's burger", 12);
-        mob.Weapon = new GameWeapon("carly's cutlass", 33);
-        mob2.Weapon = new GameWeapon("denise's dog", 5);
-
-        game.players.Add(player);
-        game.players.Add(player2);
-        game.mobs.Add(mob);
-        game.mobs.Add(mob2);
+        var game = Util.SampleGame;
         GameEvents.Instance.AttackEnd += (g, a, b) =>
         {
             Debug.LogFormat("{0} {1}/{2} attacked {3} {4}/{5}", a.name, a.Health, a.baseHealth, b.name, b.Health, b.baseHealth);
@@ -55,6 +26,41 @@ public class Main : MonoBehaviour
     }
     public void RunAnimatedTest()
     {
+        proceedButton.interactable = true;
 
+        StartCoroutine(AnimatedTest());
+    }
+    bool animatedTestProceed = false;
+    IEnumerator AnimatedTest()
+    {
+        var game = Util.SampleGame;
+        GameEvents.Instance.TargetChosen += (g, a) =>
+        {
+            Debug.LogFormat("{0} chooses target {1}", a.name, ((GameActor)a.GetAttribute(GameActor.Attribute.Target)).name);
+        };
+        GameEvents.Instance.AttackEnd += (g, a, b) =>
+        {
+            Debug.LogFormat("{0} {1}/{2} attacked {3} {4}/{5}", a.name, a.Health, a.baseHealth, b.name, b.Health, b.baseHealth);
+        };
+        GameEvents.Instance.Death += (g, a) =>
+        {
+            Debug.LogFormat("RIP {0}", a.name);
+        };
+        while(game.GameProgress == Game.Progress.InProgress)
+        {
+            Debug.Log(game.ToString());
+            game.DoTurn();
+
+            yield return new WaitUntil(() => animatedTestProceed);
+            animatedTestProceed = false;
+        }
+        Debug.LogFormat("game ended with {0}", game.GameProgress);
+
+        GameEvents.ReleaseAllListeners();
+        yield return null;
+    }
+    public void AnimatedTestProceed()
+    {
+        animatedTestProceed = true;
     }
 }
---
+++ Assets/Src/Util.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ScryptTheCrypt;
+using ScryptTheCrypt.Actions;
+
+public static class Util
+{
+    // brilliant idea from https://gamedev.stackexchange.com/questions/63721/coroutines-in-series
+    static public IEnumerator CoroutineSeries(params IEnumerator[] series)
+    {
+        foreach(var routine in series)
+        {
+            while (routine.MoveNext())
+            {
+                yield return routine.Current;
+            }
+        }
+    }
+    static public Game SampleGame
+    {
+        get
+        {
+            // run some simple tests to create and invoke a Game
+            var game = new Game(2112);
+            var player = new GameActor("alice");
+            var player2 = new GameActor("bob");
+            var mob = new GameActor("carly");
+            var mob2 = new GameActor("denise");
+
+            // set targeting and affinities
+            player.AddAction(new ActionChooseRandomTarget(Game.ActorAlignment.Mob));
+            player2.AddAction(new ActionChooseRandomTarget(Game.ActorAlignment.Mob));
+            mob.AddAction(new ActionChooseRandomTarget(Game.ActorAlignment.Player));
+            mob2.AddAction(new ActionChooseRandomTarget(Game.ActorAlignment.Player));
+
+            player.AddAction(new ActionAttack());
+            player2.AddAction(new ActionAttack());
+            mob.AddAction(new ActionAttack());
+            mob2.AddAction(new ActionAttack());
+
+            player.Weapon = new GameWeapon("alice's axe", 22);
+            player2.Weapon = new GameWeapon("bob's burger", 12);
+            mob.Weapon = new GameWeapon("carly's cutlass", 33);
+            mob2.Weapon = new GameWeapon("denise's dog", 5);
+
+            game.players.Add(player);
+            game.players.Add(player2);
+            game.mobs.Add(mob);
+            game.mobs.Add(mob2);
+
+            return game;
+        }
+    }
+}
