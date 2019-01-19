39a4f88bdc8081d357afadeba678b890385f5ae7
Kai Arnold <cdn.kai@gmail.com>
2019-01-18 16:33:32 -0800

    set up animation coroutines and calling them in series

--- Assets/Src/Main.cs
+++ Assets/Src/Main.cs
@@ -6,7 +6,7 @@ using ScryptTheCrypt;
 
 public class Main : MonoBehaviour
 {
-    [SerializeField] private UnityEngine.UI.Button proceedButton;
+    [SerializeField] private UnityEngine.UI.Button proceedButton = null;
     public void RunSmokeTest()
     {
         var game = Util.SampleGame;
@@ -26,41 +26,61 @@ public class Main : MonoBehaviour
     }
     public void RunAnimatedTest()
     {
-        proceedButton.interactable = true;
-
-        StartCoroutine(AnimatedTest());
+        StartCoroutine(AnimateTest());
     }
-    bool animatedTestProceed = false;
-    IEnumerator AnimatedTest()
+    bool waitingToProceed = false;
+    public void SetWaiting(bool waiting = true)
     {
+        waitingToProceed = waiting;
+        proceedButton.interactable = waiting;
+    }
+    IEnumerator AnimateTest()
+    {
+        var animationList = new List<IEnumerator>();
+
         var game = Util.SampleGame;
         GameEvents.Instance.TargetChosen += (g, a) =>
         {
-            Debug.LogFormat("{0} chooses target {1}", a.name, ((GameActor)a.GetAttribute(GameActor.Attribute.Target)).name);
+            animationList.Add(AnimateTargetChoice(a.name));
         };
         GameEvents.Instance.AttackEnd += (g, a, b) =>
         {
-            Debug.LogFormat("{0} {1}/{2} attacked {3} {4}/{5}", a.name, a.Health, a.baseHealth, b.name, b.Health, b.baseHealth);
+            animationList.Add(AnimateAttack(a.name));
         };
         GameEvents.Instance.Death += (g, a) =>
         {
-            Debug.LogFormat("RIP {0}", a.name);
+            animationList.Add(AnimateDeath(a.name));
         };
         while(game.GameProgress == Game.Progress.InProgress)
         {
             Debug.Log(game.ToString());
             game.DoTurn();
 
-            yield return new WaitUntil(() => animatedTestProceed);
-            animatedTestProceed = false;
+            yield return StartCoroutine(Util.CoroutineSeries(animationList.GetEnumerator()));
+
+            SetWaiting(true);
+            yield return new WaitUntil(() => !waitingToProceed);
+            SetWaiting(false);
         }
         Debug.LogFormat("game ended with {0}", game.GameProgress);
 
         GameEvents.ReleaseAllListeners();
         yield return null;
     }
-    public void AnimatedTestProceed()
+    const float animationTime = 2;
+    IEnumerator AnimateTargetChoice(string name)
     {
-        animatedTestProceed = true;
+        Debug.LogFormat("AnimateTargetChoice of {0}", name);
+        yield return new WaitForSeconds(animationTime);
+    }
+    IEnumerator AnimateAttack(string name)
+    {
+        Debug.LogFormat("AnimateAttack of {0}", name);
+        yield return new WaitForSeconds(animationTime);
+    }
+    IEnumerator AnimateDeath(string name)
+    {
+        Debug.LogFormat("AnimateDeath of {0}", name);
+        yield return new WaitForSeconds(animationTime);
     }
 }
