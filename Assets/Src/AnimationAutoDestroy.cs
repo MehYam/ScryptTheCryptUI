775de7c774e4e3676c63f687056e7aec2933adea
Kai Arnold <cdn.kai@gmail.com>
2019-01-29 09:46:57 -0800

    quiet compiler warning

--- Assets/Src/AnimationAutoDestroy.cs
+++ Assets/Src/AnimationAutoDestroy.cs
@@ -5,6 +5,6 @@
     [SerializeField] private float additionalSeconds = 0f;
     void Start()
     { 
-        Destroy(gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + additionalSeconds - 0.1f); 
+        Destroy(gameObject, GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + additionalSeconds - 0.1f); 
     }
  }
